c5bfbffd8dc9b5be3447922827e914d2124acd14
lspatnalagitbub <50782859+lspatnalagitbub@users.noreply.github.com>
2024-11-28 11:19:25 +0530

    Add files via upload

---
+++ AlertDemo.java
@@ -0,0 +1,41 @@
+package seleniumprograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.NoAlertPresentException;	
+import org.openqa.selenium.Alert;
+
+public class AlertDemo {
+	
+	public static void main(String[] args) throws NoAlertPresentException,InterruptedException  {									
+	
+		WebDriver driver = new ChromeDriver();
+	
+        
+        // Alert Message handling
+                    		
+        driver.get("https://demo.guru99.com/test/delete_customer.php");			
+                            		
+     	      	
+        driver.findElement(By.name("cusid")).sendKeys("53921");					
+        driver.findElement(By.name("submit")).submit();			
+        		
+        // Switching to Alert        
+        Alert alert = driver.switchTo().alert();	
+        		
+        // Capturing alert message.    
+        String alertMessage= driver.switchTo().alert().getText();		
+        		
+        // Displaying alert message		
+        System.out.println(alertMessage);	
+        Thread.sleep(5000);
+        		
+        // Accepting alert		
+        alert.accept();	
+        //alert.dismiss();
+        Thread.sleep(5000);
+        //driver.quit();
+    }	
+
+}
